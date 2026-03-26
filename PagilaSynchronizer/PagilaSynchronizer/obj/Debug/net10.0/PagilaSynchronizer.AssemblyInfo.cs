bc4ee8a2f949a09d67cbc7d1ef7b0c2970642cf3
Junior Nuñez <moisesmnuez@gmail.com>
2026-03-25 21:20:42 -0600

    cambios arreglando bugs

--- PagilaSynchronizer/PagilaSynchronizer/obj/Debug/net10.0/PagilaSynchronizer.AssemblyInfo.cs
+++ PagilaSynchronizer/PagilaSynchronizer/obj/Debug/net10.0/PagilaSynchronizer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PagilaSynchronizer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b105b7389d78c3e30b81241428a0bb19505b854e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PagilaSynchronizer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PagilaSynchronizer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
